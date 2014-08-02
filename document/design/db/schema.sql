4d94c8fe2b5818eccf765820b83d80d324f8a0f5
Rolyer Luo <irolyer@gmail.com>
2014-08-02 14:46:32 +0800

    update

--- document/design/db/schema.sql
+++ document/design/db/schema.sql
@@ -1,8 +1,15 @@
 ##UPDATE game SET start_time = '2014-08-01 21:35:40'
-##WHERE id > 2000;
+##WHERE id > 2000 AND id < 300
 
 ##UPDATE game SET start_time = NOW()
 ##WHERE id > 1000;
+
+##INSERT INTO `kfcms_db`.`game`(`user_name`,`name`,`start_time`,`server_name`,`url`,`category`,`gift_name`,`platform`,`gmt_created`,`gmt_modified`)
+##VALUES('admin','乱舞江山',NOW(),'双线2服','?start','Q版','开服礼包','哇塞游戏平台',NOW(),NOW());
+
+
+##INSERT INTO kfcms_db.game (`user_name`,`name`,`start_time`,`server_name`,`url`,`category`,`gift_name`,`platform`,`gmt_created`,`gmt_modified`)
+##SELECT `user_name`,`name`,`start_time`,`server_name`,`url`,`category`,`gift_name`,`platform`,`gmt_created`,`gmt_modified` FROM kfcms_db.game;
 
 SELECT id, user_name, name, start_time, server_name, url, category, gift_name, platform, gmt_created, gmt_modified 
 FROM game 
