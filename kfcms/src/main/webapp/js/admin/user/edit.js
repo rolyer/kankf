cb57f8c0f96c30f573a079387c5135fd4bf32d5d
Rolyer Luo <irolyer@gmail.com>
2014-08-17 11:40:12 +0800

    register user by admin

--- kfcms/src/main/webapp/js/admin/user/edit.js
+++ kfcms/src/main/webapp/js/admin/user/edit.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	$('#btn_save').click(function(){
 		save();
 	});
+	
+	$('#btn_reg').click(function(){
+		reg();
+	});
 });
 
 function reset(){
@@ -77,4 +81,61 @@ function save() {
 			utils.alert('#sys_msg', Message.SERVER_ERROR, 'danger');
 		}
 	});
+}
+
+function reg() {
+	
+	var account = $.trim($('#account').val());
+	var password = $.trim($('#password').val());
+	var email = $.trim($('#email').val());
+	var tel = $.trim($('#tel').val());
+	var im = $.trim($('#im').val());
+
+	if(!utils.checkFormat(account,'account')){
+		utils.alert("#sys_msg", "账号格式不正确", "danger");
+		return false;
+	}
+	
+	if(!utils.checkFormat(password,'password')){
+		utils.alert("#sys_msg", "密码格式不正确", "danger");
+		return false;
+	}
+	
+//	if($('#password').val()!=$('#password1').val()){
+//		utils.alert("#sys_msg", "两次输入的密码不一致", "danger");
+//		return false;
+//	}
+	
+	if(!utils.checkFormat(email,'email')){
+		utils.alert("#sys_msg", "邮箱格式不正确", "danger");
+		return false;
+	}
+	
+	if(tel != null && tel != "" && !utils.checkFormat(tel,'mobile') && !utils.checkFormat(tel,'tel')){
+		utils.alert("#sys_msg", "电话格式不正确", "danger");
+		return false;
+	}
+	
+	$.ajax({
+        type: "POST",
+        url: Context.PATH + '/reg.html?t='+utils.random(),
+        data : {
+			im: im,
+			tel: tel,
+            account: account,
+			password: password,
+			email: email
+        },
+        dataType:"json",
+        success: function(response, textStatus, xhr) {
+        	if(response.success){
+        		//window.location.href = "/web/index.html";
+        	} else {
+				utils.alert("#sys_msg", response.data, "danger");
+			}
+        },
+        error: function(xhr, textStatus, errorThrown) {
+			utils.alert("#sys_msg", "服务器连接错误", "danger");
+        }
+    });
 }
